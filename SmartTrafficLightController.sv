9efd5770de5cd860f4708a56669d818b5957b992
lkoopmann7352 <koopmanl@oregonstate.edu>
2026-05-27 17:32:02 -0700

    Add SmartTrafficLightController module skeleton

--- SmartTrafficLightController.sv
+++ SmartTrafficLightController.sv
@@ -1,1 +1,15 @@
+module SmartTrafficLightController #(
+        localparam CLK_FREQ = 1 // Hz
+) (
+        input logic clk,
+        input logic reset,
+        input logic side_sensor,
+        input logic ped_button,
+        input logic emergency_main,
+        input logic emergency_side
+);
+
+endmodule
+        
+        
 
