85a6c7c8ec3d8edb0de27e0f3ea9c255afaa6fbc
lkoopmann7352 <koopmanl@oregonstate.edu>
2026-05-27 17:37:04 -0700

    Implement FSM structure for SmartTrafficLightController
    Added state enumeration and base FSM logic for traffic light control.

--- SmartTrafficLightController.sv
+++ SmartTrafficLightController.sv
@@ -8,8 +8,37 @@ module SmartTrafficLightController #(
         input logic emergency_main,
         input logic emergency_side
 );
+	// Define custom type "State"
+	typedef enum logic [2:0] {
+		S0, S1, S2, S3, S4, S5, S6, S7
+	} State;
+	State cur_state, next_state;
+	
+	// Base FSM logic
+	always_ff @(posedge clk or posedge reset) begin
+		if (reset) begin
+			cur_state <= S0;
+		end else begin
+			cur_state <= next_state;
+		end
+	end
 
-endmodule
-        
-        
+	// Counter logic
+	always_ff @(posedge clk or posedge reset) begin
 
+	end
+
+	// Next-state logic
+	always_comb begin
+        	case (cur_state)
+        
+        		endcase
+        	end
+	
+	// Output encoding logic
+	always_comb begin		
+		case (cur_state)
+
+		endcase
+	end
+endmodule     
